3b37677c759d511ef1604afda6bccd1fc1700072
AvezovM <126018208+AvezovM@users.noreply.github.com>
2023-03-01 00:27:29 +0500

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.nav-left').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
